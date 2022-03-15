c186bc926d4fabb138bb31fdc98e4af2c1bb7c1b
joão vitor <93920950+vitorrcampos@users.noreply.github.com>
2022-03-15 02:10:49 -0300

    Novo estilo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,132 @@
 import React, { Component } from "react";
 import "./styles.css";
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  color: white;
+  
+  display: flex;
+  justify-content: center;
+  margin-left: 65vw;
+  font-size: 16vw;
+  color:#391463;
+  `
+
+
+const ContainerButton = styled.div `
+display: flex;
+justify-content:center;
+align-items: center;
+margin-left: 31vw;
+flex-direction: column;
+position: relative;
+left: 16vw;
+bottom: 50vw;
+`
+
+
+
+const ContainerTudo = styled.div`
+
+  text-align: center;
+  margin-top: 5vw;
+  position: relative;
+  left: -32vw;
+`
+const Resultado = styled.h2`
+text-align: center;
+  font-size: 30vw;
+  margin-left: 34vw;
+  margin-top: 20vw;
+  width: 92vw;
+  height: 40vw;
+  background-color: white;
+  border: black inset 1px;
+  border-radius: 5vw;
+  font-weight: 400`
+
+const InserirUm = styled.input`
+border: none;
+margin-top: 1vw;
+width: 30vw;
+height: 50vw;
+border-radius: 5vw;
+text-align: center;
+font-size: 5vw;
+border: black inset 1px;
+position: relative;
+left: 16vw;
+`  
+const InserirDois = styled.input`
+border: none;
+margin-top: 1vw;
+width: 30vw;
+height: 50vw;
+border-radius: 5vw;
+text-align: center;
+font-size: 5vw;
+border: black inset 1px;
+position: relative;
+left: 48vw;
+
+`  
+
+
+
+
+const Minus = styled.button`
+  width: 30vw;
+  margin-bottom: 1vw;
+  height: 12vw;
+  border: none;
+  border-radius: 50vw;
+  margin-left: 1vw;
+  background-color: Aquamarine;
+  font-size: 10vw;
+  color: white;
+cursor: pointer;
+`
+const Add = styled.button`
+width: 30vw;
+margin-bottom: 1vw;
+  height: 12vw;
+  border: none;
+  border-radius: 50vw;
+  margin-left: 1vw;
+  background-color: #E71D36;
+  font-size: 10vw;
+  color: white;
+  cursor: pointer;
+
+`
+const Divide = styled.button`
+width: 30vw;
+margin-bottom: 1vw;
+  height: 12vw;
+  border: none;
+  border-radius: 50vw;
+  margin-left: 1vw;
+  background-color: #330036;
+  font-size: 10vw;
+  color: white;
+  cursor: pointer;
+`
+const Multiply = styled.button`
+width: 30vw;
+margin-bottom: 1vw;
+  height: 12vw;
+  border: none;
+  border-radius: 50vw;
+  margin-left: 1vw;
+  background-color: #F7CB15;
+  font-size: 10vw;
+  color: white;
+  cursor: pointer;
+`
+
+const ContainerDois = styled.div`
+ 
+`
 
 export default class Calc extends Component {
   state = {
@@ -8,6 +135,7 @@ export default class Calc extends Component {
     num2: null
   };
 
+ 
   handleChange = (e) => {
     this.setState({
       num1: e.target.value
@@ -46,34 +174,34 @@ export default class Calc extends Component {
 
   render() {
     return (
-      <div>
-        <h1>CALCULADORA REACT</h1>
-        <div>
-          <h2 className="resultado">{this.state.result}</h2>
-          <input
+      <ContainerTudo>
+        <Titulo> Calculadora</Titulo>
+        <ContainerDois>
+          <Resultado>{this.state.result}</Resultado>
+          <InserirUm
             placeholder="NÚMERO"
             onChange={this.handleChange}
             type="number"
           />
-          <input
+          <InserirDois
             placeholder="NÚMERO"
             onChange={this.handleChange2}
             type="number"
           />
 
-          <div className="box-button">
-            <button onClick={this.minus}>-</button>
-            <button onClick={this.add}>+</button>
-            <button onClick={this.divide}>/</button>
-            <button onClick={this.multiply}>*</button>
+          <ContainerButton>
+            <Minus onClick={this.minus}>-</Minus>
+            <Add onClick={this.add}>+</Add>
+            <Divide onClick={this.divide}>/</Divide>
+            <Multiply onClick={this.multiply}>*</Multiply>
 
             <style>
               @import
               url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap');
             </style>
-          </div>
-        </div>
-      </div>
+          </ContainerButton>
+        </ContainerDois>
+      </ContainerTudo>
     );
   }
 }
